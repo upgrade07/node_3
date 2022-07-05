32ce4e7670a36420c510277b6460549c4e8479a1
upgrade07 <43794966+upgrade07@users.noreply.github.com>
2022-07-05 18:27:58 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+const express = require("express");
+const app = express();
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const middleware1 = function (req, res, next) {
+  console.log("middleware 1");
+  next();
+};
 
-export default App;
+app.get("/", middleware1, (req, res) => {
+  res.send("<h1>Middleware 1</h1>");
+});
+
+const middleware2 = (req, res, next) => {
+  console.log("middleware 2");
+  next();
+};
+
+app.use(middleware2);
+
+app.get("/page1", middleware1, (req, res) => {
+  res.send("<h1>page 1 with middleware 1 and middleware 2 </h1>");
+});
+
+app.get("/page2", (req, res) => {
+  res.send("<h1>page 2 middleware 2</h1>");
+});
+app.get("/page3", (req, res) => {
+  res.send("<h1>page 3 middleware 2</h1>");
+});
+app.get("/page4", (req, res) => {
+  res.send("<h1>page 4 middleware 2</h1>");
+});
+
+app.listen(6500, () => {
+  console.log("server running");
+});
